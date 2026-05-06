ce82b28872f6148f92c125b59c01add0f027b091
Pinington <georges5757c@gmail.com>
2026-05-06 22:44:16 +0200

    Making LMS work

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
-print("Hello World")
+from openai import OpenAI
+
+client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
+
+conversations = [{"role" : "system", "content" : "Speak in a very bro like manner in your answers."}]
+
+def generate(text):
+    global conversations
+    conversations.append({"role" : "user", "content" : text})
+
+    completion = client.chat.completions.create(model="model_identifier", \
+                                                messages=conversations, \
+                                                temperature=0.7)
+    
+    response = completion.choices[0].message
+    conversations.append({"role" : response.role, "content" : response.content})
+
+    return response.content
+
+
+while True:
+    user_input = str(input("You: "))
+    if user_input.lower() == "exit":
+        break
+    response = generate(user_input)
+    print(f"Bot: {response}")
+
